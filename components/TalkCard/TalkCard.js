4e1102eb5c88262bc5f4a70b790e411de4350d3e
Meena Förderer <meena.foerderer@gmail.com>
2022-11-16 16:56:04 +0100

    add dummy data

---
+++ components/TalkCard/TalkCard.js
@@ -0,0 +1,13 @@
+//import { data } from "../utils/data";
+
+export default function TalkCard() {
+  return (
+    <>
+      <h1>JURASSIC TALK 2022</h1>
+      <ul>
+        <li>First Talk</li>
+        <li>Second Talk</li>
+      </ul>
+    </>
+  );
+}
---
+++ components/utils/data.js
@@ -0,0 +1,162 @@
+const data = [
+  {
+    title:
+      "NO SWIMMING DINOSAURS HERE: NEW INSIGHTS ON MANUS-DOMINATED DINOSAUR TRACKS FROM THE MAYAN DUDE RANCH IN BANDERA, TEXAS",
+    speaker: "ADAMS",
+    session: "01 Dinosaurs",
+    time: "8:00 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "DEVELOPING A SCALING METHOD TO ESTIMATE THE CENTER OF MASS IN DINOSAURS",
+    speaker: "ALLISON",
+    session: "01 Dinosaurs",
+    time: "8:20 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "DIVERGENT STRATEGIES FOR HIGH-FIBER HERBIVORY AMONG EARLY-DIVERGING ORNITHISCHIAN DINOSAURS",
+    speaker: "BARRETT",
+    session: "01 Dinosaurs",
+    time: "8:40 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "A PATHOLOGICAL FEMUR OF AN IGUANDONTIAN DINOSAUR WITH MEDULLARY-LIKE BONE INVOLVEMENT IN THE HEALING PROCESS",
+    speaker: "BERTOZZO",
+    session: "01 Dinosaurs",
+    time: "9:00 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "HORNCORE FRACTURE IN ANCHICERATOPS (ORNITHISCHIA, CERATOPSIDAE), AND THE OCCURRENCE OF PATHOLOGICAL HORN FRACTURES IN CERATOPSIDAE",
+    speaker: "BROWN",
+    session: "01 Dinosaurs",
+    time: "09:20 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "OSTEOLOGY AND KINEMATICS OF THE PACHYCEPHALOSAURID VERTEBRAL COLUMN PROVIDES FURTHER EVIDENCE REFUTING THE AGONISTIC HEAD-BUTTING HYPOTHESIS",
+    speaker: "WOODRUFF",
+    session: "01 Dinosaurs",
+    time: "09:40 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "TOOTH REPLACEMENT CYCLES AND THE ENDOCRANIAL ANATOMY OF A NEW ORNITHOPOD DINOSAUR FROM THE LATE CRETACEOUS OF ANTARCTICA",
+    speaker: "BROWN",
+    session: "01 Dinosaurs",
+    time: "10:20 AM",
+    room: "Great Hall",
+  },
+  {
+    title: "THE CONTRIBUTION OF SOFT TISSUES TO THE NECK OF PTEROSAURS",
+    speaker: "BUCHMANN",
+    session: "01 Dinosaurs",
+    time: "10:40 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "PATTERNS OF MACROWEAR ON IN SITU TYRANNOSAURID DENTITIONS FROM THE UPPER CRETACEOUS OF NORTH AMERICA",
+    speaker: "WYENBERG-HENZLER",
+    session: "01 Dinosaurs",
+    time: "11:00 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "A NEW SPECIES OF CAENAGNATHIDAE (THEROPODA: DINOSAURIA) FROM THE OLDMAN FORMATION (CAMPANIAN: LATE CRETACEOUS) OF ALBERTA, CANADA",
+    speaker: "WILKINSON",
+    session: "01 Dinosaurs",
+    time: "11:20 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "AN UNRECOGNIZED BOUNTY: THE FOURTH DICRAEOSAURID SAUROPOD FROM THE MORRISON FORMATION (LATE JURASSIC) OF NORTH AMERICA",
+    speaker: "WHITLOCK",
+    session: "01 Dinosaurs",
+    time: "11:40 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "FROM SEA TO SKY: WATER LAUNCH AND SOARING PERFORMANCE IN THE LATE CRETACEOUS PTEROSAUR, NYCTOSAURUS",
+    speaker: "WELCH",
+    session: "01 Dinosaurs",
+    time: "13:00 AM",
+    room: "Great Hall",
+  },
+  {
+    title: "EVOLUTIONARY MODE IN DERIVED TYRANNOSAURINAE",
+    speaker: "WARSHAW",
+    session: "01 Dinosaurs",
+    time: "13:20 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "MAKING A RUN FOR IT: THE IMPACT OF FLIGHTADAPTIVE TRAITS ON NICHE PARTITIONING IN CARNIVOROUS NON-AVIAN THEROPODS",
+    speaker: "VAN VRANKEN",
+    session: "01 Dinosaurs",
+    time: "13:40 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "EVOLUTION OF THE PROPATAGIUM IN THEROPOD DINOSAURS: ANALYSES OF ARTICULATED FOSSIL SKELETONS AND EXTANT AVIAN EMBRYOS",
+    speaker: "UNO",
+    session: "01 Dinosaurs",
+    time: "14:00 AM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "A MYSTERIOUS CLUTCH OF ALTERED DINOSAUR EGGS FROM THE TWO MEDICINE FORMATION OF MONTANA",
+    speaker: "TUCKER",
+    session: "01 Dinosaurs",
+    time: "14:20 PM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "GONDWANAN THEROPOD DINOSAURS IN THE CENOMANIAN OF SOUTHWESTERN EUROPE",
+    speaker: "TORICES",
+    session: "01 Dinosaurs",
+    time: "14:40 PM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "A DIVERSE DINOSAUR EGG ASSEMBLAGE FROM THE MID-CRETACEOUS JINHUA FORMATION, ZHEJIANG PROVINCE, CHINA",
+    speaker: "TANAKA",
+    session: "01 Dinosaurs",
+    time: "15:20 PM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "A NEW LONG-NECKED POLYCOTYLID (PLESIOSAURIA: POLYCOTYLIDAE) FROM THE PIERRE SHALE OF WYOMING, U.S.A.",
+    speaker: "STREET",
+    session: "01 Dinosaurs",
+    time: "15:40 PM",
+    room: "Great Hall",
+  },
+  {
+    title:
+      "A NEW DREPANOSAUR UNGUAL MORPHOTYPE FROM THE SONSELA MEMBER OF PETRIFIED FOREST NATIONAL PARK",
+    speaker: "SODANO",
+    session: "01 Dinosaurs",
+    time: "16:00 PM",
+    room: "Great Hall",
+  },
+];
+
+export { data };
