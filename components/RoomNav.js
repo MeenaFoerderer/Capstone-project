72aa5d083154c68c7e2c1745b9212c11d9619b41
Meena Förderer <meena.foerderer@gmail.com>
2022-11-18 14:05:59 +0100

    navBar positioning

--- components/RoomNav.js
+++ components/RoomNav.js
@@ -3,9 +3,14 @@ import styled from "styled-components";
 const NavBar = styled.div`
   background-color: gray;
   height: 4em;
+  width: 100%;
   display: flex;
   justify-content: space-between;
   padding: 0.2em;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
 `;
 
 const NavButton = styled.button`
