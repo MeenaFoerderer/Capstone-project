47328632742a0322a8e58d9f8d4f811af307521a
Meena Förderer <meena.foerderer@gmail.com>
2022-12-05 17:31:10 +0100

    removed unused code

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -31,5 +31,4 @@ const GlobalStyles = createGlobalStyle`
     }
 `;
 
-// font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 export default GlobalStyles;
